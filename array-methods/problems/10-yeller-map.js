175f441f4aaa077e40dec2d48633a2c18ca0c3a7
harsh <hrrshpal@gmail.com>
2024-07-04 00:31:48 +0530

    solved problem 10

--- array-methods/problems/10-yeller-map.js
+++ array-methods/problems/10-yeller-map.js
@@ -13,7 +13,13 @@ console.log(yeller(['kiwi', 'orange', 'mango'])); // [ 'KIWI!', 'ORANGE!', 'MANG
 */
 
 let yeller = function(words) {
-  // Your code here
+  
+  let arr = words.map(function(el){
+    return el.toUpperCase()+"!"
+  })
+
+  return arr
+
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -22,4 +28,4 @@ try {
   module.exports = yeller;
 } catch (e) {
   module.exports = null;
-}
+}
