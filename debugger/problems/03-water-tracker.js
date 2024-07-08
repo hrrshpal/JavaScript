ec0bc97b5ca19bd58613dafa0d1441720eb6ced4
harsh <hrrshpal@gmail.com>
2024-07-08 20:31:23 +0530

    debugged 2 more problems

--- debugger/problems/03-water-tracker.js
+++ debugger/problems/03-water-tracker.js
@@ -12,25 +12,27 @@ A calendar is represented by multiple weeks,
 [[0, 0, 3, 1, 0, 4, 0], [1, 2, 1, 2, 1, 3, 1]].
 
 If you drank water for at least 4 days of water for every week in the calendar,
-then return true. Otherwise, return false.
+then return true. Otherwise, return false. 
 
 */
 
 function adequateWaterTracker(calendar) {
-  let noWater = 0;
-  let water = 0;
   for (let i = 0; i < calendar.length; i++) {
-    const week = calendar[i];
-    for (let j = 0; j < week.length - 1; j++) {
-      const day = calendar[j];
-      if (day === 0) {
+    let noWater=0;
+    let water=0;
+    for (let j = 0; j < calendar[i].length; j++) {
+      if (calendar[i][j] === 0) {
         noWater++;
       } else {
         water++;
       }
     }
+    if(water>noWater)
+      continue
+    else
+      return false
   }
-  return noWater < water;
+  return true
 }
 
 const calendar1 = [
@@ -53,4 +55,3 @@ const calendar3 = [
 ];
 
 console.log(adequateWaterTracker(calendar3)); // true
-
