e7de6f2d1832c09de7a0ace19bb432f52484be87
harsh <hrrshpal@gmail.com>
2024-07-22 15:52:28 +0530

    solved async-js-1 exercises

--- async-js-1/problems/04-interval-count.js
+++ async-js-1/problems/04-interval-count.js
@@ -5,19 +5,23 @@ the given callback and delay, but clear the interval after the callback
 has been executed 'amount' number of times.
 
 Hint: utilize a 'closure' to your advantage
-
 In addition to Mocha, we recommend that you test your code manually using 
 node with the examples below.
 
 Example
-
 intervalCount(function() {
     console.log('hi');
 }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
-  // Your code here
+  let fun = setInterval(function(){
+    cb()
+    amount--
+    if(amount===0){
+      clearInterval(fun)
+    }
+  },delay)
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
