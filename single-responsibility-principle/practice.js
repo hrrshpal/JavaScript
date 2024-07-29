fa84cbd5ad0bd4ac1dd6a12e893b81ff7421101d
harsh <hrrshpal@gmail.com>
2024-07-29 14:23:37 +0530

    solved SRP exercise

--- single-responsibility-principle/practice.js
+++ single-responsibility-principle/practice.js
@@ -33,33 +33,43 @@ const recipes = {
 /* DO NOT CHANGE THE CODE ABOVE */
 
 /*************************** FUNCTION TO REFACTOR ****************************/
-function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
-  // Find the recipe for the pieType specified
-  const recipe = recipes[pieType];
-  // Bake the number of pies specified by the pieQuantity
+function getRecipe(pieType){
+  return recipes[pieType];
+}
+
+function bakePies(recipe, pieType, pieQuantity){
   for (let i = 0; i < pieQuantity; i++) {
-    // Print the ingredients for each ingredient in the recipe
-    let combiningMsg = `Combining ingredients for ${pieType}: `
-    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
-    console.log(combiningMsg);
+      let combiningMsg = `Combining ingredients for ${pieType}: `
+      combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
+      console.log(combiningMsg);
+      console.log(`Baked pie ${i + 1}!`);
+    }
+}
 
-    // Print the nth pie that was baked
-    console.log(`Baked pie ${i + 1}!`);
-  }
+function costOfEachPie(recipe){
+  return recipe.reduce((prev, current) => {
+      return prev + current.cost;
+    }, recipe[0].cost);
+}
 
-  // Print the cost of each pie based on the cost of each ingredient
-  const costOfPie = recipe.reduce((prev, current) => {
-    return prev + current.cost;
-  }, recipe[0].cost);
+function totalCostOfAllPies(costOfPie,pieQuantity){
+  return costOfPie*pieQuantity
+}
+
+function totalRevenue(totalCost, profitMargin = 1.2){
+  return totalCost*profitMargin
+}
+
+function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
+  const recipe = getRecipe(pieType)
+  bakePies(recipe, pieType, pieQuantity)
+  const costOfPie = costOfEachPie(recipe)
   console.log(`Cost per pie: ${costOfPie}`);
-
-  // Calculate the total cost of all the pies
-  const totalCost = costOfPie * pieQuantity;
-
-  // Print the total revenue calculated using the given profitMargin
-  const revenue = totalCost * (profitMargin || 1.2);
+  const totalCost = totalCostOfAllPies(costOfPie,pieQuantity)
+  const revenue = totalRevenue(totalCost, profitMargin)
   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
 }
+
 
 /******************************* LOCAL TESTS *******************************/
 // bakeAndSellPies("applePie", 5, 2.5);
