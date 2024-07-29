1e05d9d9d6c07b7af85edd75e44f308d929b5450
harsh <hrrshpal@gmail.com>
2024-07-29 13:39:35 +0530

    completed dry exercise

--- DRY/practice.js
+++ DRY/practice.js
@@ -1,45 +1,29 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  if (num1 > num2) 
+    return num1 * 2;
+  else 
+    return num2 * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  if (num1 > num2) 
+    return num1 / 3;
+  else
+    return num2 / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  if (sum1 > sum2)
+    return `I ate ${sum1} tacos.`;
+  else
+    return `I ate ${sum2} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  if (weight1 < weight2)
+    return `I adopted a dog that weighs ${weight1} pounds.`;
+  else
+    return `I adopted a dog that weighs ${weight2} pounds.`;
 }
 
 
